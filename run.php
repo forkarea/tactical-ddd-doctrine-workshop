c729a1683e98021c9af08fa5578c153c4aabd2ce
Peter Decuyper <github@serial-graphics.be>
2015-06-08 13:42:46 +0200

    Added simple Person entity.

--- run.php
+++ run.php
@@ -3,5 +3,13 @@ $container = require_once __DIR__ . '/di.php';
 /** @var \Doctrine\ORM\EntityManager $entityManager */
 $entityManager = $container['entity_manager'];
 
-// Put your sample code here!
-echo "Let's rock.\n";
+
+$person1 = EmergencyRoom\Person::announce('Test announce');
+$entityManager->persist($person1);
+
+$person2 = EmergencyRoom\Person::walkIn('Test walk-in');
+$entityManager->persist($person2);
+
+$entityManager->flush();
+
+
---
+++ src/EmergencyRoom/Person.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace EmergencyRoom;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class patient
+ *
+ * @ORM\Entity
+ * @package EmergencyRoom
+ */
+class Person
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id() @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=TRUE)
+     */
+    private $indication = null;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $arrived = false;
+
+
+    /**
+     * Create a new announced person.
+     *
+     * @param string $indication
+     *
+     * @return Person
+     */
+    public static function announce($indication)
+    {
+        $person = new Person();
+        $person->changeIndication($indication);
+
+        return $person;
+    }
+
+    /**
+     *
+     */
+    public static function walkIn($indication)
+    {
+        $person = Person::announce($indication);
+        $person->setArrived();
+
+        return $person;
+    }
+
+
+    /**
+     * Get the indication message.
+     *
+     * @return string
+     */
+    public function getIndication()
+    {
+        return $this->indication;
+    }
+
+    /**
+     * Set the indication.
+     *
+     * @param string
+     */
+    public function changeIndication($indication)
+    {
+        \Assert\that($indication)
+            ->notEmpty()
+            ->string()
+            ->betweenLength(1, 1000);
+
+        $this->indication = $indication;
+    }
+
+    /**
+     * Check if the patient is arrived.
+     *
+     * @return bool
+     */
+    public function isArrived()
+    {
+        return $this->arrived;
+    }
+
+    /**
+     * Patient is arrived.
+     *
+     * @throws Exception
+     */
+    public function setArrived()
+    {
+        if ($this->arrived) {
+            throw new Exception('Patient is already arrived.');
+        }
+
+        $this->arrived = true;
+    }
+}
